9e72c08c0a7789725909a1977da2c63045e91dc8
faiq <faiq.raza@nutanix.com>
2025-04-30 17:06:22 -0700

    fix: remove chatty logs from ds

--- docker/gpu-token-copy.sh
+++ docker/gpu-token-copy.sh
@@ -3,17 +3,10 @@
 while true; do
   if ! cmp -s /config/client_configuration_token.tok /host-token/client_configuration_token.tok; then
     echo "Detected token change, propagating..."
-
-    echo "--- Differences ---"
-    diff /config/client_configuration_token.tok /host-token/client_configuration_token.tok
-    echo "-------------------"
-
     cp /config/client_configuration_token.tok /host-token/.tmp &&
     mv /host-token/.tmp /host-token/client_configuration_token.tok &&
     chmod 644 /host-token/client_configuration_token.tok &&
     echo "Token updated successfully at $(date)"
-  else
-    echo "No change detected"
   fi
   sleep 2
 done
