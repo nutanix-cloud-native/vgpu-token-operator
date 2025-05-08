3636151efdc8d9f4123ba13b19753e1675650ff2
faiq <faiq.raza@nutanix.com>
2025-05-08 10:20:09 -0700

    fix: change group name

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -19,7 +19,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the nkp v1alpha1 API group.
 // +kubebuilder:object:generate=true
-// +groupName=nkp.nutanix.com
+// +groupName=vgpu-token.nutanix.com
 package v1alpha1
 
 import (
@@ -29,7 +29,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "nkp.nutanix.com", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "vgpu-token.nutanix.com", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
